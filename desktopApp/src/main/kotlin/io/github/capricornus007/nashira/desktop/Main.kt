2ebf84dc82f4f1d61fbdc9ad98edaa18ac1dbfad
Capricornus007 <Capricornus007@users.noreply.github.com>
2026-09-03 14:53:32 +0900

    fix: 桌面視窗預設尺寸 1100x760

--- desktopApp/src/main/kotlin/io/github/capricornus007/nashira/desktop/Main.kt
+++ desktopApp/src/main/kotlin/io/github/capricornus007/nashira/desktop/Main.kt
@@ -1,8 +1,11 @@
 package io.github.capricornus007.nashira.desktop
 
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import io.github.capricornus007.nashira.App
 
 fun main() = application {
@@ -10,6 +13,7 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "Nashira",
         icon = painterResource("nashira-icon.png"),
+        state = rememberWindowState(size = DpSize(1100.dp, 760.dp)),
     ) {
         App(defaultDark = true)
     }
